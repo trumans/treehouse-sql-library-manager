f84b4c865ab520de77db09bb38be5d608242e5a2
Truman Smith <trumanwsmith@gmail.com>
2019-04-26 00:53:07 -0700

    implements search and pagination features. expand readme.md

--- README.md
+++ README.md
@@ -1,21 +1,49 @@
 
-# Main Features #
+# Routes and Features #
+
+/ - root redirects to /books
+/books (get) - list books in database
+  supports query strings:
+    p=page - 'page' is the page# from the results to display. This first page is
+      is displayed if the key omitted or 'page' is not an integer.
+    s=value - 'value' is the case-insensitive search value applied to the title,
+      author, genre or year fields. Records are not filtered if omitted or null.
+/books/new (get) - form to create a book
+/books/new (post) - create a book using data from the form
+/books/# (get) - form display, update or delete a book, where # is record id
+/books/# (post) - update the book record using data from the form
+/books/#/delete (post) - delete the book record
+/server-error - display an error 599 on server error page
 
 # Exceeds Requirements Features #
 
+/books contains a search field which filters records as described above in 
+  s=value query string
+/books page contains buttons to display the previous and next page of books. The
+  'previous' button is suppressed on page 1, and the 'next' button is suppressed 
+  on a partial page. Initially the page size is only 5 books to facilitate in 
+  demonstrating the buttons. The page size can be changed by editing the
+  pageSize variable at the top of /routes/books.js
+
 # To Install #
+
 1. Download the project and install dependencies with "npm install".
 2. Launch the app with "npm start", which calls app.js.
+3. In a browser open the URL "localhost:3000"
 
 # Project Structure #
+
+The application uses Express to manage routes, Pug to generate html, and 
+Sequelize to access a local sqlite database.
 
 app.js        - starts the application on the server. contains error handling routes
 config folder - project configurations, such as db connections used by sequelize
 library.db    - sqlite database containing book data
 migrations folder - not used
 model folder  - object used by sequelize to interact with database 
+package.json  - npm configuration file
 public folder - static assets, such as css stylesheet
+README.md     - this file
 routes folder - routes for books pages
 seeders folder - not used
 views folder  - pug templates for rendering html
-
--- routes/books.js
+++ routes/books.js
@@ -1,45 +1,48 @@
 const express = require('express');
 const router = express.Router();
 
-const Book = require('../models').Book;
 const SqlOp = require('sequelize').Op;
+
+const Book = require('../models').Book;
 
 const pageSize = 5;
 
-// Return parameters for error page to display a 500 error
+// Return parameters to display a 500 error on error page
 function status_500_params(message) {
 	return {status: 500, message: message, title: "Server Error"}
 }
 
-// Return parameters for error page to display response status code and message
+// Return parameters to display response status code and message on error page 
 function response_status_params(res) {
 	return {status: res.statusCode, message: res.statusMessage, 
 			title: "Server Error"}
 }
 
-// Create a new book form
+// Create a new book - entry form
 router.get('/books/new', (req, res, next) => {
 	res.render('new-book', { book: Book.build(), title: 'New Book' });
 });
 
 // Create a new book - post action
-router.post('/books/new', (req, res, next) => {
+router.post('/books/new', (req, res) => {
 	Book.create(req.body)
 		.then( function() {res.redirect('/books')})
 		// If validation error then re-render page with error messages
 		.catch( function(err) {
+			// if validation error then re-render with error messages
         	if (err.name === "SequelizeValidationError") {
             	var book = Book.build(req.body);
     			book.id = req.params.id
             	res.render('new-book', 
               		{ book: book, title: 'New Book', errors: err.errors });
+            // any other errors 
         	} else {
 				res.render('error', status_500_params(err));
         	}
         });
 });
 
-// Display/update/delete a book form
+// Display a book with options to update or delete
 router.get('/books/:id', (req, res, next) => {
 	Book.findByPk(req.params.id).then( function(book) { 
 		if ( book ) {
@@ -52,7 +55,7 @@ router.get('/books/:id', (req, res, next) => {
 });
 
 // Delete a book - post action
-router.post('/books/:id/delete', (req, res, next) => {
+router.post('/books/:id/delete', (req, res) => {
 	Book.findByPk(req.params.id)
 		.then( function(book) { 
 			if (book) {
@@ -67,7 +70,7 @@ router.post('/books/:id/delete', (req, res, next) => {
 });
 
 // Update a book - post action
-router.post('/books/:id', (req, res, next) => {
+router.post('/books/:id', (req, res) => {
 	Book.findByPk(req.params.id)
 		.then( function(book) { return book.update(req.body); })
 		.then( function(book) { 
@@ -83,31 +86,42 @@ router.post('/books/:id', (req, res, next) => {
     			book.id = req.params.id
             	res.render('update-book', 
               		{ book: book, title: book.title, errors: err.errors });
+            // any other errors
         	} else {
 				res.render('error', status_500_params(err));
         	}});
 });
 
-// List all books by page
+// List all books by page with search filter
 router.get('/books', (req, res) => {
-	const page = parseInt(req.query.p);  // page# from URL query params
-	var opts = { offset: ( isNaN(page) ) ? 0 : (page-1) * pageSize, 
-				 limit:  pageSize
-			   }
-	// search criteria, if in query string
-	if (req.query.s != undefined) {
-		const search = `%${req.query.s}%`;
-		opts.where = { 
-				[SqlOp.or]: [
-					{title:  {[SqlOp.like]: search}}, 
-					{author: {[SqlOp.like]: search}}, 
-					{genre:  {[SqlOp.like]: search}}, 
-					{year:   {[SqlOp.like]: search}} 
-				]}
-	}
+	var page = parseInt(req.query.p);
+	var search = req.query.s;  
 
-	Book.findAll(opts).then( function(books) {
-  		res.render('index', {books: books, title: 'Books'});
+	// set find options
+	var findOpts = {};
+	if (isNaN(page)) { page = 1 }
+	findOpts.offset = (page-1) * pageSize;
+	findOpts.limit =  pageSize;
+	// search criteria from query string	
+	if ( search != undefined) {
+		const searchEx = `%${search}%`;
+		findOpts.where = { 
+				[SqlOp.or]: [
+					{title:  {[SqlOp.like]: searchEx}}, 
+					{author: {[SqlOp.like]: searchEx}}, 
+					{genre:  {[SqlOp.like]: searchEx}}, 
+					{year:   {[SqlOp.like]: searchEx}} 
+				]}
+	} 
+
+	// find books and render results
+	Book.findAll(findOpts).then( function(books) {
+		var renderOpts = { books: books, title: 'Books' }
+		renderOpts.search = search || ''
+		if (page > 1) { renderOpts.prevPage = page - 1 }
+		if (books.length == pageSize) { renderOpts.nextPage = page + 1 }
+
+  		res.render('index', renderOpts);
 	});
 });
 
